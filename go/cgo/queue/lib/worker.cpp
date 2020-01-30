26ee49350cfa2c94e7757d4a8e8670e998181fc3
GauthamBanasandra <gautham.bangalore@gmail.com>
2020-01-30 10:13:24 +0530

    [cgo] Add v8 to queue

--- go/cgo/queue/lib/worker.cpp
+++ go/cgo/queue/lib/worker.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <libplatform/libplatform.h>
+#include <ostream>
 #include <thread>
+#include <v8.h>
 
 #include "worker.h"
+
+Worker::Worker() : messages_{5} {}
+
+Worker::~Worker() {
+  script_.Reset();
+  context_.Reset();
+  isolate_->Dispose();
+  v8::V8::Dispose();
+  v8::V8::ShutdownPlatform();
+}
 
 void Worker::Start() {
   std::thread runner([this]() {
@@ -15,4 +28,42 @@ void Worker::Start() {
 void Worker::Process() {
   const auto value = messages_.Pop();
   std::cout << "Worker got value : " << value << std::endl;
+
+  v8::V8::InitializeICUDefaultLocation("");
+  v8::V8::InitializeExternalStartupData("");
+  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+  v8::V8::InitializePlatform(platform.get());
+  v8::V8::Initialize();
+
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+  v8::Isolate *isolate = v8::Isolate::New(create_params);
+  {
+    v8::Isolate::Scope isolate_scope(isolate);
+    v8::HandleScope handle_scope(isolate);
+    v8::Local<v8::Context> context = v8::Context::New(isolate);
+    v8::Context::Scope context_scope(context);
+    {
+      auto source_str = R"(
+function add(a, b) {
+  return a + b;
+}
+add(4, 5);
+      )";
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8(isolate, source_str,
+                                  v8::NewStringType::kNormal)
+              .ToLocalChecked();
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+      v8::String::Utf8Value result_utf8(isolate, result);
+      std::cout << *result_utf8 << std::endl;
+    }
+  }
+  isolate->Dispose();
+  v8::V8::Dispose();
+  v8::V8::ShutdownPlatform();
+  delete create_params.array_buffer_allocator;
 }
--- go/cgo/queue/lib/worker.h
+++ go/cgo/queue/lib/worker.h
@@ -1,16 +1,17 @@
 #ifndef WORKER_H
 #define WORKER_H
 
+#include <string>
+#include <v8.h>
+
 #include "blocking-queue.h"
 
 class Worker {
 public:
-  Worker() : messages_{5} {}
+  Worker();
+  ~Worker();
 
-  void Process(const int value)
-  {
-    messages_.Push(value);
-  }
+  void Process(const int value) { messages_.Push(value); }
   void Start();
 
 private:
@@ -18,6 +19,10 @@ private:
 
   bool stop_{false};
   BlockingQueue messages_;
+  v8::Isolate *isolate_{nullptr};
+  v8::Persistent<v8::Context> context_;
+  v8::Persistent<v8::Script> script_;
+  v8::Isolate::CreateParams isolate_params_;
 };
 
 #endif
