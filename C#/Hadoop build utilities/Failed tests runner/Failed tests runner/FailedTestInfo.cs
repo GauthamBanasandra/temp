4ad7eacc2f769d67d00a1cf52a4ab9c1c9bd0cb2
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-06-29 22:43:31 +0530

    [C#] Add FailedTestInfo

---
+++ C#/Hadoop build utilities/Failed tests runner/Failed tests runner/FailedTestInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Failed_tests_runner
+{
+    class FailedTestInfo
+    {
+        public string TestClass { get; set; }
+        public string TestName { get; set; }
+        public string TestFilePath { get; set; }
+    }
+}
--- C#/Hadoop build utilities/Failed tests runner/Failed tests runner/Program.cs
+++ C#/Hadoop build utilities/Failed tests runner/Failed tests runner/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 
 namespace Failed_tests_runner
@@ -7,6 +8,28 @@ namespace Failed_tests_runner
     class Program
     {
         static void Main(string[] args)
+        {
+            var failedTests = new string[]
+            {
+                "org.apache.hadoop.hdfs.server.namenode.ha.TestPipelinesFailover.testFailoverRightBeforeCommitSynchronization",
+                "org.apache.hadoop.yarn.sls.appmaster.TestAMSimulator.testAMSimulatorWithNodeLabels[0]",
+                "org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.TestFairScheduler.testMoveWouldViolateMaxAppsConstraints",
+                "org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.TestFairSchedulerPreemption.testRelaxLocalityPreemptionWithNoLessAMInRemainingNodes[MinSharePreemptionWithDRF]",
+                "org.apache.hadoop.hdfs.server.blockmanagement.TestBlockTokenWithDFSStriped.testRead",
+                "org.apache.hadoop.yarn.applications.distributedshell.TestDistributedShell.testDSShellWithOpportunisticContainers",
+                "org.apache.hadoop.yarn.applications.distributedshell.TestDistributedShell.testDSShellWithEnforceExecutionType"
+            };
+
+            var info = failedTests.Select(e => e.Split('.')).Select(e => new FailedTestInfo() { TestName = e.Last(), TestClass = e.SkipLast(1).Last() });
+            foreach (var t in info)
+            {
+                Console.WriteLine($"{t.TestClass}\t{t.TestName}");
+            }
+        }
+
+
+
+        private void SomeMethod()
         {
             ProcessStartInfo processStartInfo = new ProcessStartInfo("mvn", "--version")
             {
