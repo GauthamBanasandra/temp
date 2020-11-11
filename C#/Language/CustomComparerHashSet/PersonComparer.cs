a5cb62534b861721c3da0cdce6114858ff991944
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-11-11 20:36:22 +0530

    [C#] Add custom comparer

---
+++ C#/Language/CustomComparerHashSet/PersonComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomComparerHashSet
+{
+    public class PersonComparer : IEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y)
+        {
+            if (x == null || y == null)
+            {
+                throw new Exception("Person object must not be null");
+            }
+            return x.Id.Equals(y.Id);
+        }
+
+        public int GetHashCode(Person obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
---
+++ C#/Language/CustomComparerHashSet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomComparerHashSet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var people = new HashSet<Person>(new PersonComparer())
+            {
+                new Person() {Id = "abcd", Name = "a"},
+                new Person() {Id = "efgh", Name = "b"},
+                new Person() {Id = "abcd", Name = "a"}
+            };
+
+            Console.WriteLine($"People count = {people.Count}");
+        }
+    }
+}
