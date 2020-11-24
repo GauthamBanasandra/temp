c4bb69dbda0d29b127d9b26029bcbae660509fa4
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-11-24 21:26:59 +0530

    [Java] Add Person
    Signed-off-by: Gautham Banasandra <gautham.bangalore@gmail.com>

---
+++ Java/src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+    private int age;
+    private Gender gender;
+
+    public Person(String name, int age, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
