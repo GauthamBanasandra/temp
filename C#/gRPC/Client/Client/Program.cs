28f1bd0e6371d38bdcc219883b70b452ede57962
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-12-11 13:54:46 +0530

    [C#] Add gRPC client

---
+++ C#/gRPC/Client/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using MathClient;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:50052", ChannelCredentials.Insecure);
+
+            var operands = new Operands() { Operand1 = 1, Operand2 = 2 };
+            var client = new Calculator.CalculatorClient(channel);
+
+            var result = await client.AddAsync(operands);
+            Console.WriteLine(result.Number_);
+        }
+    }
+}
