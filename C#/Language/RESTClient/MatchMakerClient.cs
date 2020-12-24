12695a9182110af5d4f6b441078463d2b48479ef
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-12-24 20:05:38 +0530

    [C#] Add RESTClient

---
+++ C#/Language/RESTClient/MatchMakerClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace RESTClient
+{
+    class MatchMakerClient
+    {
+        private const string Url = "http://localhost:9090/match";
+
+        public static string GetMatch(string xmlRequestBody)
+        {
+            var request = WebRequest.Create(Url) as HttpWebRequest;
+            var bytes = Encoding.ASCII.GetBytes(xmlRequestBody);
+            request.ContentType = "text/xml; encoding='utf-8'";
+            request.ContentLength = bytes.Length;
+            request.Method = "POST";
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(bytes, 0, bytes.Length);
+            requestStream.Close();
+
+            var response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            var responseStream = response.GetResponseStream();
+            var responseStr = new StreamReader(responseStream).ReadToEnd();
+            return responseStr;
+        }
+    }
+}
---
+++ C#/Language/RESTClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using XMLCreator;
+
+namespace RESTClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boy = new Person
+            {
+                Id = "A123",
+                Name = "a",
+                PhoneNumber = 123,
+                PlaceOfBirth = "Bangalore",
+                DateAndTimeOfBirth = DateTime.Now,
+                Gender = Gender.Male,
+                SessionId = "bjf"
+            };
+
+            var girl = new Person
+            {
+                Id = "B456",
+                Name = "b",
+                PhoneNumber = 456,
+                PlaceOfBirth = "Bangalore",
+                DateAndTimeOfBirth = DateTime.Now,
+                Gender = Gender.Female,
+                SessionId = "ckvj"
+            };
+
+            var xmlRequestBody = XmlCreator.Create(boy, girl);
+            var matchResponse = MatchMakerClient.GetMatch(xmlRequestBody);
+            Console.WriteLine(matchResponse);
+        }
+    }
+}
