fca3ae9fbfc6b09bc98797f8aba92f290d4fffd6
Gautham Banasandra <gautham.bangalore@gmail.com>
2021-01-09 23:00:54 +0530

    [C#] Add "GeoTrie"

---
+++ C#/Language/GeoTrie/GeoLocation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileHelpers;
+
+namespace GeoTrie
+{
+    [DelimitedRecord("\t")]
+    class GeoLocation
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string AsciiName { get; set; }
+        public string? AlternateNames { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string? FeatureClass { get; set; }
+        public string? FeatureCode { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryCode2 { get; set; }
+        public string? Admin1Code { get; set; }
+        public string? Admin2Code { get; set; }
+        public string? Admin3Code { get; set; }
+        public string? Admin4Code { get; set; }
+        public long? Population { get; set; }
+        public long? Elevation { get; set; }
+        public string? DigitalElevationModel { get; set; }
+        public string Timezone { get; set; }
+        public string? ModificationDate { get; set; }
+    }
+}
---
+++ C#/Language/GeoTrie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FileHelpers;
+using Gma.DataStructures.StringSearch;
+
+namespace GeoTrie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileHelperEngine = new FileHelperEngine<GeoLocation>();
+            var geoLocations = fileHelperEngine.ReadFile(@"C:\Users\Gautham\Downloads\IN\IN.txt");
+            Console.WriteLine($"Count={geoLocations.Length}");
+
+            var indexer = new UkkonenTrie<GeoLocation>(geoLocations.Length);
+            foreach (var geoLocation in geoLocations)
+            {
+                indexer.Add(geoLocation.AsciiName, geoLocation);
+            }
+            Console.WriteLine("Finished indexing");
+
+            var result = indexer.Retrieve("Bangalore");
+            foreach (var geoLocation in result)
+            {
+                Console.WriteLine($"Id={geoLocation.Id} Name={geoLocation.Name}");
+            }
+        }
+    }
+}
