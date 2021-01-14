fae90d294ceded183d965e937b78fc88c07f7044
Gautham Banasandra <gautham.bangalore@gmail.com>
2021-01-14 22:04:05 +0530

    [C#] Fix issue with PrependNewLine

---
+++ C#/Files/RecursiveTouch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Console = System.Console;
+
+namespace RecursiveTouch
+{
+    class Program
+    {
+        public static void TouchAll(string path, ISet<string> exclude = null)
+        {
+            if (!Directory.Exists(path) || (exclude != null && exclude.Contains(path)))
+            {
+                return;
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                Console.WriteLine(file);
+                File.WriteAllText(file, $"\n{File.ReadAllText(file)}");
+            }
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                TouchAll(directory, exclude);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Need exactly 1 parameter - the path to the root directory");
+                return;
+            }
+
+            var exclusions = new HashSet<string>
+            {
+                @"C:\Users\Gautham\projects\apache\wsl\hadoop\.git",
+                @"C:\Users\Gautham\projects\apache\wsl\hadoop\.github"
+            };
+
+            TouchAll(args[0], exclusions);
+        }
+    }
+}
---
+++ C#/Files/ShowDirectoryName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace ShowDirectoryName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Gautham\projects\apache\wsl\hadoop\.git";
+            Console.WriteLine(Path.GetDirectoryName(path));
+        }
+    }
+}
