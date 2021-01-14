b2a547032c81a5fe4b68d49169af6de18d94335a
Gautham Banasandra <gautham.bangalore@gmail.com>
2021-01-14 20:19:14 +0530

    [C#] Add Files

---
+++ C#/Files/DirWalker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace DirWalker
+{
+    class Program
+    {
+        public static void DirWalk(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                Console.WriteLine(file);
+            }
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                DirWalk(directory);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Gautham\projects\apache\wsl\hadoop";
+            DirWalk(path);
+        }
+    }
+}
---
+++ C#/Files/Files/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Gautham\Documents\doc-chat.txt";
+            File.WriteAllText(path, $"\n{File.ReadAllText(path)}");
+        }
+    }
+}
